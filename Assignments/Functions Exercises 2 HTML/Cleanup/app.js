10bd9a7551926baa50389356be717913350ec5b0
JustBobbyyy <bobbyezenwelu@yahoo.com>
2022-04-05 11:43:28 -0400

    Update

--- Assignments/Functions Exercises 2 HTML/Cleanup/app.js
+++ Assignments/Functions Exercises 2 HTML/Cleanup/app.js
@@ -3,23 +3,31 @@
 //3/31/2022
 //Make Random 
 
+//Write a web page to test the function - one button and input, and one div
 let txtClean= document.getElementById("txtClean");
 let dvRemove= document.getElementById("Remove");
 
+
+    
+//create a function that removes the "#" whenever presses the button 
+//Find a way so that it removes "#" everytime the user puts it in the input
 function By(){
-    let userName = txtClean.value;
-    let story = `${userName}  # `;
-    return story = story.replace("# ", " ");
-    
-    
-}
+   let userName = txtClean.value;
+    let story = `${userName}`;
+    //console.log(story)
+    return story.replace(/#/g," ");
 
+  
+    
+};
+  
 
+//create a function that brings out the button
 function Up(){
     let res = By()
     dvRemove.innerHTML = res
-    
-}
+    console.log(res)
+};
 
 
 // variable.value
